5b06296363b2edd04995bd235bb06d252b7fef7d
Matt <iko172302@gmail.com>
2019-01-02 00:03:19 -0700

    test

---
+++ pizza.py
@@ -0,0 +1,8 @@
+def pizza(size,*toppings): 
+	print('Your pizza will be ' +str(size) + '.') 
+	
+	for topping in toppings:
+		print('It will include the following toppings: ' + str(toppings ))
+
+pizza(14,'pepporoni, cheese, chile') 
+print ('per your request') 
